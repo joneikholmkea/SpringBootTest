216b9517bafb5e849d410f1be28f7faaabd9fd1b
joneikholmkea <jone@kea.dk>
2022-11-17 13:00:13 +0100

    second

--- src/main/java/j/vilegeritimen/LegeKontroller.java
+++ src/main/java/j/vilegeritimen/LegeKontroller.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -28,7 +29,7 @@ public class LegeKontroller {
     }
 
     @PostMapping("/addUser")
-    public ResponseEntity<Map<String,String>> addUser(User user){
+    public ResponseEntity<Map<String,String>> addUser(@RequestBody User user){
         Map<String,String> map = new HashMap<>();
         map.put("message", "du kan godt");
         if(user.getUsername().length()>0) {
--- src/test/java/j/vilegeritimen/ViLegerITimenApplicationTests.java
+++ src/test/java/j/vilegeritimen/ViLegerITimenApplicationTests.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@DataJpaTest() //
+@DataJpaTest() // bruger H2 automatisk
 class ViLegerITimenApplicationTests {
     private MockMvc mockMvc;
     private UserService userService;
@@ -34,13 +35,27 @@ class ViLegerITimenApplicationTests {
 
     @Test
     public void addUser() throws Exception {
-        mockMvc.perform(post("/addUser")
-                .param("id", "123")
-                .param("username", "anna")
-                .param("password", "444"))
+//        mockMvc.perform(post("/addUser")
+//                .param("id", "123")
+//                .param("username", "anna")
+//                .param("password", "444"))
+//                .andExpect(status().isOk())
+//                .andDo(print());
+
+        mockMvc.perform(post("/addUser") // integration test
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {"id":"1",
+                     "username":"anna",
+                     "password": "123"
+                    }
+                """)
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
 
+
+// .content("{\"username\" : \"anna\"}")
 //        mockMvc.perform(post("/addUser")
 //                        .param("id", "123")
 //                        .param("username", "anna")
