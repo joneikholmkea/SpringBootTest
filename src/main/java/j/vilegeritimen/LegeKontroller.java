e6fd530ba628de0f7bb8588cf55b8dd6d38fa226
joneikholmkea <jone@kea.dk>
2022-12-12 11:22:24 +0100

    Revert "second"
    This reverts commit 216b9517bafb5e849d410f1be28f7faaabd9fd1b.

--- src/main/java/j/vilegeritimen/LegeKontroller.java
+++ src/main/java/j/vilegeritimen/LegeKontroller.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -29,7 +28,7 @@ public class LegeKontroller {
     }
 
     @PostMapping("/addUser")
-    public ResponseEntity<Map<String,String>> addUser(@RequestBody User user){
+    public ResponseEntity<Map<String,String>> addUser(User user){
         Map<String,String> map = new HashMap<>();
         map.put("message", "du kan godt");
         if(user.getUsername().length()>0) {
--- src/test/java/j/vilegeritimen/ViLegerITimenApplicationTests.java
+++ src/test/java/j/vilegeritimen/ViLegerITimenApplicationTests.java
@@ -6,14 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@DataJpaTest() // bruger H2 automatisk
+@DataJpaTest() //
 class ViLegerITimenApplicationTests {
     private MockMvc mockMvc;
     private UserService userService;
@@ -35,27 +34,13 @@ class ViLegerITimenApplicationTests {
 
     @Test
     public void addUser() throws Exception {
-//        mockMvc.perform(post("/addUser")
-//                .param("id", "123")
-//                .param("username", "anna")
-//                .param("password", "444"))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-
-        mockMvc.perform(post("/addUser") // integration test
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                    {"id":"1",
-                     "username":"anna",
-                     "password": "123"
-                    }
-                """)
-                .accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(post("/addUser")
+                .param("id", "123")
+                .param("username", "anna")
+                .param("password", "444"))
                 .andExpect(status().isOk())
                 .andDo(print());
 
-
-// .content("{\"username\" : \"anna\"}")
 //        mockMvc.perform(post("/addUser")
 //                        .param("id", "123")
 //                        .param("username", "anna")
